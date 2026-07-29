ea72853044177e87f6e17d48d8af1bb25b772c98
Divyang <divyang.dhameliya@zeuslearning.com>
2026-07-29 17:34:29 +0530

    feat: Implemented menu component with pizza listing with add to cart

--- pizza-store-client/src/app/app-routing.module.ts
+++ pizza-store-client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { OrderDetailsComponent } from './components/order-details/order-details.component';
+import { AdminComponent } from './components/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/menu', pathMatch: 'full' },
+  { path: 'menu', component: MenuComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'orders/:id', component: OrderDetailsComponent },
+  { path: 'admin', component: AdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- pizza-store-client/src/app/components/menu/menu.component.ts
+++ pizza-store-client/src/app/components/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pizza } from '../../models/pizza.model';
+import { PizzaService } from '../../services/pizza.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  pizzas: Pizza[] = [];
+  loading = true;
+  error = '';
 
-  constructor() { }
+  constructor(
+    private pizzaService: PizzaService,
+    private cartService: CartService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loadPizzas();
   }
 
+  loadPizzas(): void {
+    this.loading = true;
+    this.pizzaService.getActivePizzas().subscribe({
+      next: (data) => {
+        this.pizzas = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load menu. Is the API running?';
+        this.loading = false;
+        console.error(err);
+      }
+    });
+  }
+
+  addToCart(pizza: Pizza): void {
+    this.cartService.addToCart(pizza, 1);
+    alert(`${pizza.name} added to cart`);
+  }
 }
