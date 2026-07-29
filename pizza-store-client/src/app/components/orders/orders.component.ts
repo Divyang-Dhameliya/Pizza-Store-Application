e95d3c79dd0bd4492d212ab83c3ad35a71607165
Divyang <divyang.dhameliya@zeuslearning.com>
2026-07-29 18:12:04 +0530

    feat: Implemented Order component

--- pizza-store-client/src/app/components/orders/orders.component.ts
+++ pizza-store-client/src/app/components/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../models/order.model';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  orders: Order[] = [];
+  loading = true;
+  error = '';
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loadOrders();
   }
 
+  loadOrders(): void {
+    this.loading = true;
+    this.orderService.getAllOrders().subscribe({
+      next: (data) => {
+        this.orders = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load orders.';
+        this.loading = false;
+        console.error(err);
+      }
+    });
+  }
 }
