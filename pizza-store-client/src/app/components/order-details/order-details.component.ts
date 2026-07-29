45bbdd9587e7a976b21807209c0c754b585421b5
Divyang <divyang.dhameliya@zeuslearning.com>
2026-07-29 18:12:57 +0530

    feat: Implemented order-details component with navbar

--- pizza-store-client/src/app/components/order-details/order-details.component.ts
+++ pizza-store-client/src/app/components/order-details/order-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Order, OrderDetailItem } from '../../models/order.model';
+import { OrderService } from '../../services/order.service';
 
 @Component({
   selector: 'app-order-details',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.component.css']
 })
 export class OrderDetailsComponent implements OnInit {
+  order: Order | null = null;
+  items: OrderDetailItem[] = [];
+  loading = true;
+  error = '';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private orderService: OrderService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const orderId = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadOrderDetails(orderId);
   }
 
+  loadOrderDetails(orderId: number): void {
+    this.loading = true;
+    this.orderService.getOrderDetails(orderId).subscribe({
+      next: (data) => {
+        this.order = data.order;
+        this.items = data.items;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load order details.';
+        this.loading = false;
+        console.error(err);
+      }
+    });
+  }
 }
